9c84479f525897cbf67d348d100ea6d87ea317bf
BeenaAnn <31032677+BeenaAnn@users.noreply.github.com>
2017-08-22 03:50:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one',function (req,res){
+    res.send('This is Article-One and you are being served Article-One.');
+});
+app.get('/article-two',function (req,res){
+    res.send('This is Article-Two and you are being served Article-Two.');
+});
+app.get('/article-three',function (req,res){
+    res.send('This is Article-Three and you are being served Article-Three.');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
